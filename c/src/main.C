ca3d01ae9000fd0d3d2876a10197e061cefdd105
Nick Chapman <nic@amadido.co.uk>
2022-11-27 16:35:18 +0000

    lower case

--- c/src/main.C
+++ c/src/main.C
@@ -13,13 +13,15 @@ typedef int i32;
 const u32 physical_width = 1920;
 const u32 physical_height = 1080;
 
-const u32 world_width = 1024;
-const u32 world_height = 800;
+const u32 world_width = 1920; //1024;
+const u32 world_height = 1080; //800;
 
-const u32 border = 50;
+const u32 border = 0; //50
 
 const u32 virtual_width = world_width + border;
 const u32 virtual_height = world_height + border;
+
+const u32 life_scale = 4;
 
 static u64 wallclock_time() { //in micro-seconds
   struct timeval tv;
@@ -33,9 +35,9 @@ static void init_life(void);
 void prepare_life(void);
 void step_world(void);
 
+const u32 million = 1000000;
 
 void print_stats_maybe(u64 runtime) {
-  const u32 million = 1000000;
   const u32 prints_per_second = 1;
   static u32 frame = 0;
   static u32 prints = 0;
@@ -65,7 +67,7 @@ int main() {
     print_stats_maybe(runtime);
     prepare_life();
     blit();
-    if (runtime > 2000000) {
+    if (runtime > 10 * million) {
       step_world();
     }
   };
@@ -76,7 +78,12 @@ static u32 screen[virtual_height * virtual_width] = {};
 
 void blit() {
   FILE* fp = fopen("/dev/fb0","w");
-  //fwrite(&screen,sizeof(u32),physical_size,fp);
+  fwrite(&screen,sizeof(u32),physical_width*physical_height,fp);
+  fclose(fp);
+}
+
+void blitV() {
+  FILE* fp = fopen("/dev/fb0","w");
   const u32 off_v = (physical_height - virtual_height) / 2;
   const u32 off_h = (physical_width - virtual_width) / 2;
   for (u32 y = 0; y < virtual_height; y++) {
@@ -91,8 +98,6 @@ const u32 black = 0x00000000;
 const u32 white = 0x00ffffff;
 const u32 blue  = 0x000000ff;
 const u32 grey  = 0x007f7f7f;
-
-const u32 life_scale = 8;
 
 const u32 life_width = world_width/life_scale;
 const u32 life_height = world_height/life_scale;
@@ -176,9 +181,105 @@ void place(xy loc, xy* elems) {
   }
 }
 
+const char* alphaA[] =
+  {"                                                 ii    jj                        ",
+   "       bb             dd       fff         hh    ii    jj    kk    ll            ",
+   "       bb             dd       fff         hh                kk    ll            ",
+   " aaaaa bbbbb ccccc ddddd eeeee ff    ggggg hhhhh ii    jj    kk kk ll    mmmmmmmm",
+   " aaaaa bbbbb ccccc ddddd eeeee ff    ggggg hhhhh ii    jj    kk kk ll    mmmmmmmm",
+   "    aa bb bb cc cc dd dd ee ee fff   gg gg hh hh ii    jj    kk kk ll    mm mm mm",
+   " aaaaa bb bb cc    dd dd eeeee fff   gg gg hh hh ii    jj    kkkk  ll    mm mm mm",
+   " aaaaa bb bb cc    dd dd eeeee ff    gg gg hh hh ii    jj    kkkk  ll    mm mm mm",
+   " aa aa bb bb cc cc dd dd ee    ff    ggggg hh hh ii    jj    kk kk ll    mm mm mm",
+   " aaaaa bbbbb ccccc ddddd eeeee ff    ggggg hh hh ii    jj    kk kk ll    mm mm mm",
+   " aaaaa bbbbb ccccc ddddd eeeee ff       gg hh hh ii    jj    kk kk ll    mm mm mm",
+   "                                     ggggg            jjj                        ",
+   "                                     ggggg            jjj                        "};
+
+const char* alphaN[] =
+  {"                                                               ",
+   "                                     tt                        ",
+   "                                     tt                        ",
+   " nnnnn ooooo ppppp qqqqq rrr   sssss ttt   uu uu vv vv ww ww ww",
+   " nnnnn ooooo ppppp qqqqq rrr   sssss ttt   uu uu vv vv ww ww ww",
+   " nn nn oo oo pp pp qq qq rr    ss    tt    uu uu vv vv ww ww ww",
+   " nn nn oo oo pp pp qq qq rr    sssss tt    uu uu vv vv ww ww ww",
+   " nn nn oo oo pp pp qq qq rr    sssss tt    uu uu vv vv ww ww ww",
+   " nn nn oo oo pp pp qq qq rr       ss tt    uu uu vvvvv ww ww ww",
+   " nn nn ooooo ppppp qqqqq rr    sssss ttt   uuuuu  vvv  wwwwwwww",
+   " nn nn ooooo ppppp qqqqq rr    sssss ttt   uuuuu  vvv  wwwwwwww",
+   "             pp       qq                                       ",
+   "             pp       qq                                       "};
+
+const char* alphaX[] =
+  {"                  ",
+   "                  ",
+   "                  ",
+   " xx xx yy yy zzzzz",
+   " xx xx yy yy zzzzz",
+   " xx xx yy yy    zz",
+   "  xxx  yy yy zzzzz",
+   "  xxx  yy yy zzzzz",
+   " xx xx yyyyy zz   ",
+   " xx xx yyyyy zzzzz",
+   " xx xx    yy zzzzz",
+   "       yyyyy      ",
+   "       yyyyy      "};
+
+u32 char_pixel_width(char c) { // including 1 pixel gap (for all but j descender)
+  //                a b c d e f g h i j k l m n o p q r s t u v w x y z
+  if (c==' ') return 6;
+  static u32 x[] = {6,6,6,6,6,4,6,6,3,3,6,3,9,6,6,6,6,4,6,4,6,6,9,6,6,6};
+  return x[c-'a'];
+}
+
+void place_alpha(xy loc, char c) {
+  if (c == ' ') return;
+  assert(c >= 'a' && c <= 'z');
+  const u32 char_height = 13;
+  const u32 char_description_width = 6;
+  const u32 char_width = char_pixel_width(c);
+  const char** alpha = (c<'n')?alphaA :c<'x'?alphaN :alphaX;
+  u32 offset = c -    ((c<'n')?    'a':c<'x'?    'n':    'x');
+  for (u32 j = 0; j < char_height; j++) {
+    const char* line = alpha[j];
+    for (u32 i = 0; i < char_width; i++) {
+      char desc = line[i + offset * char_description_width] ;
+      bool check = (desc == c) || (desc == ' ');
+      if(!check) {
+        printf("check failed: '%c' -> '%c'\n",c,desc);
+        printf("i=%d, j=%d, offset=%d\n",i,j,offset);
+        printf("line=%s\n",line);
+      }
+      assert(check);
+      bool on = (desc == c);
+      if (on) {
+        u32 x = (loc.x + i) % life_width;
+        u32 y = (loc.y + j) % life_height;
+        u32 cell = y * life_width + x;
+        world[cell] = 1;
+      }
+    }
+  }
+}
+
+xy addXY(xy a, xy b) {
+  return { a.x + b.x, a.y + b.y };
+}
+
+void place_string(xy loc, const char* s) {
+  while (char c = *s++) {
+    place_alpha(loc,c);
+    loc = addXY(loc,{(i32)char_pixel_width(c),0});
+  }
+}
+
+
 void init_life() {
   //place({5,7},gliderDR);
   //place({23,15},gliderDR);
   //place({10,40},gliderDL);
-  place({10,10},gosperGun);
+  //place({10,10},gosperGun);
+  place_string({50,110},"abcdefghijklmnopqrstuvwxyz");
+  place_string({50,150},"in xxxx john horton conway discovered the game of life");
 }
